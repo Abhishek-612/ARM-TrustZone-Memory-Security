2d4142669af4ba6a19feac8456b1d1580d8deb6c
Abhishek-612 <abhirevadekar12@gmail.com>
2023-08-29 14:06:31 -0400

    First commit

---
+++ memory_tests/matrix_multiply_ta/ta/.matrix_multiply_ta.o.d
@@ -0,0 +1,20 @@
+matrix_multiply_ta.o: matrix_multiply_ta.c \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/tee_internal_api.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/compiler.h \
+ /home/abhi06/Desktop/optee/toolchains/aarch64/lib/gcc/aarch64-buildroot-linux-gnu/10.4.0/include/stddef.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/tee_api_defines.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/tee_api_types.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/inttypes.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/stdint.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/limits.h \
+ /home/abhi06/Desktop/optee/toolchains/aarch64/lib/gcc/aarch64-buildroot-linux-gnu/10.4.0/include/stdbool.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/trace.h \
+ /home/abhi06/Desktop/optee/toolchains/aarch64/lib/gcc/aarch64-buildroot-linux-gnu/10.4.0/include/stdarg.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/trace_levels.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/tee_api_compat.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/tee_internal_api_extensions.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/stdio.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/tee_api_defines_extensions.h \
+ ../ta/include/matrix_multiply_ta.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/string.h \
+ /home/abhi06/Desktop/optee/optee_os/out/arm/export-ta_arm64/include/sys/cdefs.h
